aa539382064417ce1020df647746f9d04a6f5b18
Usborn Ocampo <usborn116@gmail.com>
2023-06-29 16:33:13 -0700

    refactored some code

--- app/controllers/plaid_credentials_controller.rb
+++ app/controllers/plaid_credentials_controller.rb
@@ -1,4 +1,6 @@
 class PlaidCredentialsController < ApplicationController
+
+    before_action :find_credential, :create_config
     require 'plaid'
     require 'dotenv'
 
@@ -10,20 +12,7 @@ class PlaidCredentialsController < ApplicationController
         find_credential.update!(plaid_credential_params)
     end
 
-    def create_config
-        configuration = Plaid::Configuration.new
-        configuration.server_index = Plaid::Configuration::Environment[ENV['PLAID_ENV']]
-        configuration.api_key['PLAID-CLIENT-ID'] =  ENV['PLAID_CLIENT_ID']
-        configuration.api_key['PLAID-SECRET'] = ENV['PLAID_SECRET']
-        api_client = Plaid::ApiClient.new(configuration)
-        client = Plaid::PlaidApi.new(api_client)
-        client
-    end
-
     def create_link_token
-        @credential = find_credential
-        client = create_config
-
         request = Plaid::LinkTokenCreateRequest.new(
             {
             user: { client_user_id: current_user.id.to_s },
@@ -35,31 +24,21 @@ class PlaidCredentialsController < ApplicationController
             webhook: 'https://webhook.example.com'
             }
         )
-        response = client.link_token_create(request)
+        response = @client.link_token_create(request)
         @credential.update!(:link_token => response.link_token.to_json)
-        print @credential
         render json: response
     end
 
     def exchange_public_token
-        @credential = find_credential
-        client = create_config
-
         request = Plaid::ItemPublicTokenExchangeRequest.new({public_token: params[:public_token]})
-        response = client.item_public_token_exchange(request)
+        response = @client.item_public_token_exchange(request)
         @credential.update!(access_token: response.access_token)
         @credential.update!(item_id: response.item_id)
         @access_token
     end
 
     def sync_transactions
-        @credential = find_credential
-        print "ID: #{@credential.id}, Access: #{@credential.access_token}"
-        
         begin
-            client = create_config
-            cursor = @credential.cursor
-        
             # New transaction updates since "cursor"
             added = []
             modified = []
@@ -69,10 +48,10 @@ class PlaidCredentialsController < ApplicationController
             request = Plaid::TransactionsSyncRequest.new(
                 {
                 access_token: @credential.access_token,
-                cursor: cursor
+                cursor: @credential.cursor
                 }
             )
-            response = client.transactions_sync(request)
+            response = @client.transactions_sync(request)
             # Add this page of results
             added += response.added
             modified += response.modified
@@ -81,7 +60,7 @@ class PlaidCredentialsController < ApplicationController
             # Update cursor to the next cursor
             puts response.next_cursor
             puts response.has_more
-            @credential.cursor = response.next_cursor
+            @credential.update!(cursor: response.next_cursor)
 
             # Return the 8 most recent transactions
             added.each do |t| 
@@ -112,14 +91,46 @@ class PlaidCredentialsController < ApplicationController
         end
     end
 
+    def get_balances
+        request = Plaid::AccountsBalanceGetRequest.new({ access_token: @credential.access_token })
+        response = @client.accounts_balance_get(request)
+        accounts = response.accounts
+
+        accounts.each do |a| 
+            acct = Account.find_or_create_by(account_id: a.account_id)
+            txn.update!(
+                account_id: a.account_id,
+                available: a.balances.available,
+                current: a.balances.current,
+                limit: a.balances.limit,
+                last_four: a.mask,
+                name: a.name,
+                official_name: a.official_name,
+                type: a.type,
+                subtype: a.subtype,
+                user_id: current_user.id
+            )
+        end
+        render json: accounts.sort_by(&name).last(8).map(&:to_hash)
+    end
+
     private
 
     def find_credential
-        PlaidCredential.find_or_create_by(user_id: current_user.id)
+        @credential = PlaidCredential.find_or_create_by(user_id: current_user.id)
     end
 
     def plaid_credential_params
         require(:plaid_credential).permit(:link_token, :access_token, :item_id, :cursor)
+    end
+
+    def create_config
+        configuration = Plaid::Configuration.new
+        configuration.server_index = Plaid::Configuration::Environment[ENV['PLAID_ENV']]
+        configuration.api_key['PLAID-CLIENT-ID'] =  ENV['PLAID_CLIENT_ID']
+        configuration.api_key['PLAID-SECRET'] = ENV['PLAID_SECRET']
+        api_client = Plaid::ApiClient.new(configuration)
+        @client = Plaid::PlaidApi.new(api_client)
     end
 
 end
