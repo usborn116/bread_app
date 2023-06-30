2f00b12a8a9a547162338849b9a3451d261bf513
Usborn Ocampo <usborn116@gmail.com>
2023-06-30 13:03:25 -0700

    ordered txns

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ class TransactionsController < ApplicationController
 
   # GET /transactions or /transactions.json
   def index
-    @transactions = Transaction.where(user_id: current_user.id)
+    @transactions = Transaction.where(user_id: current_user.id).sort_by(&:date)
   end
 
   # GET /transactions/1 or /transactions/1.json
