22670f17e87f0025f5f5cf96cef74525c4b2bcca
Usborn Ocampo <usborn116@gmail.com>
2023-08-17 13:06:32 -0700

    preselect current value in Select form fields

--- app/javascript/components/Input.jsx
+++ app/javascript/components/Input.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({type, name, placeHolder, val = '', options = null}) => {
 
     if (type == 'select') return (
-        <select name={name}>
+        <select name={name} defaultValue={val}>
             {options.map(op => <option key={op.id} value={op.id}>{op.month_name || op.name}</option> )}
         </select>
 
